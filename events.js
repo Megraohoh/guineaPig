3aa4f147bb05942855cc6e4f276db88bbede4d34
Meghan Debity <mdebity@gmail.com>
2017-03-04 13:09:36 -0600

    added variables and first function

--- events.js
+++ events.js
@@ -1,11 +1,65 @@
-// When any section is clicked the output target text should be "You clicked on the {text of the section} section"
-// When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
-// When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
-// When you type characters into the input field, the output element should mirror the text in the input field.
-// When you click the "Add color" button, the guinea-pig element's text color should change to blue.
-// When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
-// When you click the "Capture it" button, the guinea-pig element should have a border added to it.
-// When you click the "Rounded" button, the guinea-pig element's border should become rounded.
-// The first section's text should be bold.
-// The last section's text should be bold and italicized.
-// Make the buttons stop appearing next to each other as inline elements. Change them into block elements.
+//*****VARIABLES*****
+var document.getElementById("page-header");
+var document.getElementById("page-title");
+var document.getElementById("output-target");
+var document.getElementById("guinea-pig");
+var document.getElementById("add-color");
+var document.getElementById("make-large");
+var document.getElementById("add-border");
+var document.getElementById("add-rounding");
+
+//*****FUNCTIONS*****
+
+document.addEventListener("click", function(e){
+	writeToOutputTarget(e.target.innerText);
+});
+
+
+
+
+
+
+// When any section is clicked the output target text should be 
+//"You clicked on the {text of the section} section"
+//***click
+
+
+// When the mouse is over the h1 tag, the output element should 
+//contain the text "You moved your mouse over the header".
+//***mouseover
+
+
+// When the mouse leaves the h1 tag, the output element should 
+//contain the text "You left me!!".
+//***mouseout
+
+// When you type characters into the input field, the output 
+//element should mirror the text in the input field.
+
+// When you click the "Add color" button, the guinea-pig element's 
+//text color should change to blue.
+//***click/button
+
+
+
+// When you click the "Hulkify" button, the guinea-pig element's 
+//font size should become much larger.
+//***click/button
+
+document.addEventListener("click", function(e){
+
+});
+
+// When you click the "Capture it" button, the guinea-pig element 
+//should have a border added to it.
+//***click/button
+
+
+
+// When you click the "Rounded" button, the guinea-pig element's 
+//border should become rounded.
+//***click/button
+
+
+
+
